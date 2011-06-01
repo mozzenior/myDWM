3648c0e0641dba7ae23c6eddd51a86af5e9f28b5
Mozzenior Morisato <morisato3711@gmail.com>
2011-06-02 07:56:25 +0800

    Add mirror-tile.

--- dwm.c
+++ dwm.c
@@ -196,6 +196,7 @@ static void killclient(const Arg *arg);
 static void manage(Window w, XWindowAttributes *wa);
 static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
+static void mirrortile(Monitor *m);
 static void monocle(Monitor *m);
 static void movemouse(const Arg *arg);
 static Client *nexttiled(Client *c);
@@ -1176,6 +1177,38 @@ maprequest(XEvent *e) {
 		return;
 	if(!wintoclient(ev->window))
 		manage(ev->window, &wa);
+}
+
+void
+mirrortile(Monitor *m) {
+	int x, y, h, w, mh;
+	unsigned int i, n;
+	Client *c;
+
+	for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	if(n == 0)
+		return;
+	/* master */
+	c = nexttiled(m->clients);
+	mh = m->mfact * m->wh;
+	resize(c, m->wx, m->wy, m->ww - 2 * c->bw, (n == 1 ? m->wh : mh) - 2 * c->bw, False);
+	if(--n == 0)
+		return;
+	/* tile stack */
+	x = m->wx;
+	y = (m->wy + mh > c->y + c->h) ? c->y + c->h + 2 * c->bw : m->wy + mh;
+	w = m->ww / n;
+	if(w < bh)
+		w = m->ww;
+	h = (m->wy + mh > c->y + c->h) ? m->wy + m->wh - y : m->wh - mh;
+
+	for(i = 0, c = nexttiled(c->next); c; c = nexttiled(c->next), i++) {
+		resize(c, x, y,
+				((i + 1 == n) ?  m->wx + m->mw - x - 2 * c->bw : w - 2 * c->bw),
+				h - 2 * c->bw, False);
+		if(w != m->ww)
+			x = c->x + WIDTH(c);
+	}
 }
 
 void
