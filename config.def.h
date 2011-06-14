0b8ffb2e8c4da2d1e2c1cbf2c355722d49f7ee65
Mozzenior Morisato <morisato3711@gmail.com>
2011-06-14 20:18:06 +0800

    Map Meta+Shift+p to launch gmrun.

--- config.def.h
+++ config.def.h
@@ -46,11 +46,13 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *gmruncmd[] = { "gmrun", NULL };
 static const char *termcmd[]  = { "roxterm", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = gmruncmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
