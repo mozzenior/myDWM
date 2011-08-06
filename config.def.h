59cc465969f6ac26aea8bcaeae726e2a9ea2726d
Mozzenior Morisato <morisato3711@gmail.com>
2011-08-06 20:15:55 +0800

    Remove a less-frequently-used function from config.
    Signed-off-by: Mozzenior Morisato <morisato3711@gmail.com>

--- config.def.h
+++ config.def.h
@@ -80,7 +80,6 @@ static Key keys[] = {
 /* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
     /* click                event mask      button          function        argument */
-    { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
     { ClkWinTitle,          0,              Button2,        zoom,           {0} },
     { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
     { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
